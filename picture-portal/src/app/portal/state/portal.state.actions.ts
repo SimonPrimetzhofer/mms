ccfd6a50faefda39501febe87d1bf710ce04c23d
SimonPrimetzhofer <simon.primetzhofer@live.at>
2021-06-04 11:35:18 +0200

    #16 popover menu

--- picture-portal/src/app/portal/state/portal.state.actions.ts
+++ picture-portal/src/app/portal/state/portal.state.actions.ts
@@ -1,5 +1,13 @@
+import { PortalUser } from "src/app/api/models";
+
 export class LoadPictures {
     static readonly type = '[Portal] Load pictures';
 
     constructor() {}
+}
+
+export class SetUser {
+    static readonly type = '[Portal] Set user';
+
+    constructor(public user: PortalUser) {}
 }
