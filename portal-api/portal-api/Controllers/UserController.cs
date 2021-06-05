3fd871268ffedaaee2bf6a73191f3ef9ac94c541
2767mr <2767mr@gmx.at>
2021-06-05 13:52:53 +0200

    Added login to api
    #13

---
+++ portal-api/portal-api/Controllers/UserController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using portal_api.Context;
+using portal_api.Model;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portal_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static readonly PasswordHasher<PortalUser> hasher = new();
+
+        private readonly IConfiguration _config;
+        private readonly PictureContext _context;
+
+        public UserController(PictureContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+
+        [HttpGet]
+        [Authorize]
+        public Task<PortalUser> Get()
+        {
+            return User.Get(_context);
+        }
+
+        [AllowAnonymous]
+        [HttpPost(nameof(Login))]
+        public async Task<ActionResult<LoggedInDTO>> Login([FromBody] LoginDTO data)
+        {
+            PortalUser user = await AuthenticateUser(data);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            string tokenString = GenerateJSONWebToken(user);
+            return new LoggedInDTO() { Token = tokenString };
+        }
+
+        [AllowAnonymous]
+        [HttpPost(nameof(Signup))]
+        public async Task<ActionResult<LoggedInDTO>> Signup([FromBody] SignupDTO data)
+        {
+            if(await _context.PortalUsers.AnyAsync(u => u.Username == data.Username))
+            {
+                return Unauthorized();
+            }
+
+            PortalUser result = new()
+            {
+                Username = data.Username,
+                Mail = data.Mail,
+                IsAdmin = false,
+                PictureEntries = Array.Empty<PictureEntry>()
+            };
+
+            result.Password = hasher.HashPassword(result, data.Password);
+
+            await _context.PortalUsers.AddAsync(result);
+            try
+            {
+                await _context.SaveChangesAsync();
+            } 
+            catch
+            {
+                return Unauthorized();
+            }
+
+            string tokenString = GenerateJSONWebToken(result);
+
+            return new LoggedInDTO() { Token = tokenString };
+        }
+
+        private async Task<PortalUser> AuthenticateUser(LoginDTO login)
+        {
+            PortalUser user = await _context.PortalUsers.SingleOrDefaultAsync(u => u.Username == login.UserName);
+            if (user == null || user.Password == null)
+            {
+                return null;
+            }
+
+            PasswordVerificationResult result = hasher.VerifyHashedPassword(user, user.Password, login.Password);
+
+            switch (result)
+            {
+                case PasswordVerificationResult.SuccessRehashNeeded:
+                    user.Password = hasher.HashPassword(user, login.Password);
+                    await _context.SaveChangesAsync();
+                    return user;
+                case PasswordVerificationResult.Success:
+                    return user;
+                case PasswordVerificationResult.Failed:
+                default:
+                    return null;
+            }
+        }
+
+        private string GenerateJSONWebToken(PortalUser userInfo)
+        {
+            SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            SigningCredentials credentials = new(securityKey, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new(_config["Jwt:Issuer"], _config["Jwt:Issuer"], new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, userInfo.UserId.ToString())
+            }, null, DateTime.Now.AddDays(1), credentials); 
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- portal-api/portal-api/Model/PortalUser.cs
+++ portal-api/portal-api/Model/PortalUser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace portal_api.Model
@@ -21,6 +22,7 @@ namespace portal_api.Model
         public string Mail { set; get; }
 
         [Required]
+        [JsonIgnore]
         public string Password { set; get; }
 
         [Required]
