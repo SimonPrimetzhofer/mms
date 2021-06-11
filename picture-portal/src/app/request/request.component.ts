d679e907d1b1b3924127df081e583b62314a7b47
2767mr <2767mr@gmx.at>
2021-06-11 20:34:43 +0200

    Added API call to request page
    #12

--- picture-portal/src/app/request/request.component.ts
+++ picture-portal/src/app/request/request.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestService } from '../api/services';
+import notify from "devextreme/ui/notify";
 
 @Component({
   selector: 'app-request',
@@ -7,11 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestComponent implements OnInit {
 
-  details = {}
+  details = {
+    title: '',
+    description: '',
+    imageURL: ''
+  }
 
-  constructor() { }
+  constructor(
+    private requestService: RequestService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  submit(event: Event): void {
+    event.preventDefault();
+    
+    this.requestService.requestPost({
+      body: {
+        title: this.details.title,
+        description: this.details.description,
+        relatedPicture: 1,
+      }
+    }).subscribe(
+      () => {
+        this.details = {title: '', description: '', imageURL: ''}
+        notify("Request submitted.")
+      },
+      (error) => notify("Error while submitting request: " + error?.message, "error", 5000)
+    );
+  }
 }
