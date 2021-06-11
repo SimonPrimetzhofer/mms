d780bd3ba3d8a54b8b724c438de894224d805d13
SimonPrimetzhofer <simon.primetzhofer@live.at>
2021-06-11 23:19:05 +0200

    #8 added editor

--- picture-portal/src/app/editing/editing.component.ts
+++ picture-portal/src/app/editing/editing.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import ImageEditor from 'tui-image-editor';
 
 @Component({
   selector: 'app-editing',
   templateUrl: './editing.component.html',
   styleUrls: ['./editing.component.scss']
 })
-export class EditingComponent implements OnInit {
+export class EditingComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('container') container: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    new ImageEditor(this.container.nativeElement, {
+      cssMaxHeight: 800,
+      cssMaxWidth: 1000,
+      usageStatistics: false,
+      includeUI: {
+        uiSize: {
+          width: '100%',
+          height: '800px'
+        }
+      }
+    })
   }
 
 }
