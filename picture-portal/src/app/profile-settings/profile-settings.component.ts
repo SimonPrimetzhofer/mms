882584de79dd1ea222815d1dd1635e4298753b4f
2767mr <2767mr@gmx.at>
2021-06-13 10:51:33 +0200

    Added profile settings UI

--- picture-portal/src/app/profile-settings/profile-settings.component.ts
+++ picture-portal/src/app/profile-settings/profile-settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../api/services';
+import notify from 'devextreme/ui/notify';
 
 @Component({
   selector: 'app-profile-settings',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileSettingsComponent implements OnInit {
 
-  constructor() { }
+  details = {
+    username: '',
+    email: '',
+    oldPassword: '',
+    newPassword: '',
+    confirmNewPassword: ''
+  };
 
-  ngOnInit(): void {
+  passwordComparison = () => {
+    return this.details.newPassword;
   }
 
+
+  constructor(
+    private userService: UserService,
+  ) { }
+
+  ngOnInit(): void {
+    this.userService.userGet()
+      .subscribe(user => {
+        this.details.username = user.username;
+        this.details.email = user.mail;
+      });
+  }
+  
+  update(event: Event): void {
+    event.preventDefault();
+
+    this.userService.userPost({
+      body: {
+        mail: this.details.email,
+        username: this.details.username
+      }
+    }).subscribe(
+      () => notify('User settings updated!'),
+      err => notify('Error while updating settings: ' + err?.message, 'error', 5000));
+  }
+
+  updatePassword(event: Event): void {
+    event.preventDefault();
+
+    this.userService.userPost_1({
+      body: {
+        oldPassword: this.details.oldPassword,
+        newPassword: this.details.newPassword,
+      }
+    }).subscribe(
+      () => notify('User settings updated!'),
+      err => notify('Error while updating settings: ' + err?.message, 'error', 5000));
+  }
+
+  deleteUser(event: Event): void {
+    event.preventDefault();
+
+    this.userService.userDelete()
+      .subscribe(
+        () => notify('User deleted!'),
+        err => notify('Error while deleting user: ' + err?.message, 'error', 5000));
+  }
 }
